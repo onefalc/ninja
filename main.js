1b606b95640999da7978d57d41f4ea554c37cf3f
John Falcone <John_Falcone@ncci.com>
2018-06-25 14:52:32 -0400

    chapter 3

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('Welcome to Quiz Ninja!');
+const quiz = [
+	["Superman", "Clark Kent"],
+	["Wonder Woman", "Diana Prince"],
+	["Batman", "Bruce Wayne"]
+];
+
+let score = 0;
+
+for (const [question, answer] of quiz) {
+	const response = prompt(`What is ${question}'s real name?`);
+	if (response.toLowerCase() === answer.toLowerCase()) {
+		alert('Correct!');
+		score++;
+	}
+	else {
+		alert(`Wrong! The correct answer was ${answer}`);
+	}
+}
+
+// At the end of the game, report the player's score
+alert(`Game over, you scored ${score} point${score !== 1 ? 's' : ''}`);
