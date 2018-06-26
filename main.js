9300d399fcf0849b10c264dc2c8adfd4f4ba5c08
John Falcone <John_Falcone@ncci.com>
2018-06-26 13:30:11 -0400

    Chapter 5

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 const quiz = [
-	["Superman", "Clark Kent"],
-	["Wonder Woman", "Diana Prince"],
-	["Batman", "Bruce Wayne"]
+	{ name: "Superman", realName: "Clark Kent" },
+	{ name: "Wonder Woman", realName: "Diana Prince"},
+	{ name: "Batman", realName: "Bruce Wayne"}
 ];
 
-function start(quiz) {
-	let score = 0;
+const game = {
+	start(quiz) {
+		this.questions = [...quiz];
+		this.score = 0;
 
-	// Main game loop
-	for (const [question, answer] of quiz) {
-		const response = ask(`What is ${question}'s real name?`);
-		check(response, answer);
-	}
-	// End of main game loop
+		// Main game loop
+		for (const question of this.questions) {
+			this.question = question;
+			this.ask();
+		}
 
-	gameOver();
+		// End of main game loop
+		this.gameOver();
+	},
 
-	// Function declarations
-	function ask(question) {
-		return prompt(question);
-	}
+	ask() {
+		const question = `What is ${this.question.name}'s real name?`;
+		const response = prompt(question);
+		this.check(response);
+	},
 
-	function check(response, answer) {
+	check(response) {
+		const answer = this.question.realName;
 		if (response.toLowerCase() === answer.toLowerCase()) {
 			alert('Correct!');
-			score++;
+			this.score++;
 		}
 		else {
 			alert(`Wrong! The correct answer was ${answer}`);
 		}
-	}
+	},
 
-	function gameOver() {
-		alert(`Game over, you scored ${score} point${score !== 1 ? 's' : ''}`)
+	gameOver() {
+		alert(`Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
 	}
 }
 
 // At the end of the game, report the player's score
-start(quiz)
+game.start(quiz);
