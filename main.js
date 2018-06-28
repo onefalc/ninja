ec73993622fdc5fe09c780c9ff2852bc0ef1ac5c
John Falcone <John_Falcone@ncci.com>
2018-06-28 14:19:36 -0400

    Chapter 6

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const quiz = [
 	{ name: "Batman", realName: "Bruce Wayne"}
 ];
 
+const view = {
+	score: document.querySelector('#score strong'),
+	question: document.getElementById('question'),
+	result: document.getElementById('result'),
+	info: document.getElementById('info'),
+	render (target, content, attributes) {
+		for (const key in attributes) {
+			target.setAttribute(key, attributes[key]);
+		}
+		target.innerHTML = content;
+	}
+};
+
 const game = {
 	start(quiz) {
 		this.questions = [...quiz];
@@ -21,6 +34,7 @@ const game = {
 
 	ask() {
 		const question = `What is ${this.question.name}'s real name?`;
+		view.render(view.question, question);
 		const response = prompt(question);
 		this.check(response);
 	},
@@ -28,16 +42,20 @@ const game = {
 	check(response) {
 		const answer = this.question.realName;
 		if (response.toLowerCase() === answer.toLowerCase()) {
+			view.render(view.result, 'Correct!', {'class': 'correct'});
 			alert('Correct!');
 			this.score++;
+			view.render(view.score, this.score);
 		}
 		else {
+			view.render(view.result, 'Wrong! The correct answer was ${answer}', {'class': 'wrong'});
 			alert(`Wrong! The correct answer was ${answer}`);
 		}
 	},
 
 	gameOver() {
-		alert(`Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
+		view.render(view.info, `Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+		alert(`Game over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
 	}
 }
 
